f635aa3309df1807ade8cf8d719b17ea2cfb926c
Michael Perry <lmperry@stanford.edu>
2019-11-18 14:31:30 -0800

    Added support for session, acquisition, and subject label as inputs to fwrender.sh

--- utils/pbrtv3/flywheel/fw_upload_renderings.py
+++ utils/pbrtv3/flywheel/fw_upload_renderings.py
@@ -22,26 +22,38 @@ if __name__ == "__main__":
     input_directory = sys.argv[2]
     upload_project = sys.argv[3]
     acquisition_label = sys.argv[4]
+    session_label = sys.argv[5]
+    subject_label = sys.argv[6]
 
-    # 0. Initialize Flywheel
+    # If the session_label was not passed into fwrender.sh, then we will have
+    # received a 'null' string here. Thus we check and if 'null' is the label,
+    # we will fall back to using the acquistiion label to set the session label.
+    if session_label == 'null':
+        session_label = acquisition_label.split("_")[0]
+
+    # If the subject_label was not passed into fwrender.sh, then we will have
+    # received a 'null' string here. Thus we check and if 'null' is the label,
+    # we will fall back to using 'renderings' as the session label.
+    if subject_label == 'null':
+        subject_label = 'renderings'
+
+
+    # 0. Initialize Flywheel Object
     fw = flywheel.Flywheel(api_key)
 
     # 1. UPLOAD all INPUT files that are .dat or .txt
     upload_files = list(glob.glob(os.path.join(input_directory, '*.dat')))
     upload_files.extend(glob.glob(os.path.join(input_directory, '*.txt')))
 
-    # Create the acquisition in the session to upload.
-    session_label = acquisition_label.split("_")[0]
-
-    # Find the session if it already exists
+    # Find the session if it already exists. If the session does not exist we create it.
     project_sessions = fw.get_project_sessions(upload_project)
-    this_session = [ x for x in project_sessions if x['label'] == session_label and x['subject']['label'] == 'renderings' ]
+    this_session = [ x for x in project_sessions if x['label'] == session_label and x['subject']['label'] == subject_label ]
 
     if this_session and len(this_session) == 1:
         session_id = this_session[0]['_id']
         print('Using existing session...')
     else:
-        session_id = fw.add_session(flywheel.Session(project=upload_project, label=session_label, subject={'code': 'renderings', 'label': 'renderings'}, timestamp=datetime.datetime.utcnow().replace(tzinfo=pytz.timezone('America/Los_Angeles')).isoformat()))
+        session_id = fw.add_session(flywheel.Session(project=upload_project, label=session_label, subject={'code': subject_label, 'label': subject_label}, timestamp=datetime.datetime.utcnow().replace(tzinfo=pytz.timezone('America/Los_Angeles')).isoformat()))
 
     acquisition_id = fw.add_acquisition(flywheel.Acquisition(session=session_id, label=acquisition_label, timestamp=datetime.datetime.utcnow().replace(tzinfo=pytz.timezone('America/Los_Angeles')).isoformat()))
     print('Uploading files...')
--- utils/pbrtv3/flywheel/fwrender.sh
+++ utils/pbrtv3/flywheel/fwrender.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 ############
-# INPUTS
+# Positional Arguments
 # 1 - API_KEY
-# 2 - Input acquisition ID (download all files in that acquisition) (TODO: ADD TO MATLAB CODE)
+# 2 - Input acquisition ID (we download all files in that acquisition)
 # 3 - String containing the acquisition IDs and filenames
-# 4 - Output project ID(renderings) in Flywheel in which to place the outputs.
-#     This may be a new session????
+# 4 - Output project ID in Flywheel in which to place the outputs.
+# 5 - Session Label - this will be the label used for the Session in FLYWHEEL
+# 6 - Acquisition label - this will the label used for the acquisition in FLYWHEEL
+# 7 - Subject Label - this will be used to set the subject label/code in FLYWHEEL
+
+# Example usage:
+#    fwrender.sh <API_KEY> \
+#                5dd319a5fd5f730040fe7f8b \
+#                '5dd319a5fd5f730040fe7f8b city4_9:30_v0.0_f40.00front_o270_materials.pbrt' \
+#                5c2df7a933cc940062d70d6c \
+#                city4_9:30_v0.0_f40.00front_o270 \
+#                city4 \
+#                renderings
 ############
 
 
@@ -35,13 +46,33 @@ for PBRT_FILE in ${PBRT_FILES}; do
 
 done
 
-ACQ_NAME=${INPUT_FILE_NAME}
-if [[ ${ACQ_NAME} == *"_mesh"* ]]; then
-  ACQ_NAME=$(basename ${ACQ_NAME} _mesh)
-elif [[ ${INPUT_FILE_NAME} == *"_depth"* ]]; then
-  ACQ_NAME=$(basename ${ACQ_NAME} _depth)
+# Handle the session label parameter
+if [[ -z "$5" ]]; then
+  session_label='null'
+else
+  session_label="$5"
 fi
+
+# Handle the acquisition label parameter
+if [[ -z "$6" ]]; then
+  acquisition_label=${INPUT_FILE_NAME}
+  if [[ ${acquisition_label} == *"_mesh"* ]]; then
+    acquisition_label=$(basename ${acquisition_label} _mesh)
+  elif [[ ${INPUT_FILE_NAME} == *"_depth"* ]]; then
+    acquisition_label=$(basename ${acquisition_label} _depth)
+  fi
+else
+  acquisition_label="$6"
+fi
+
+# Handle the subject label parameter
+if [[ -z "$7" ]]; then
+  subject_label='null'
+else
+  subject_label="$7"
+fi
+
 
 # UPLOAD BACK TO FLYWHEEL
 # 1 - Find the output_file and label_file (if it exists) and upload to the container in $4
-python /code/fw_upload_renderings.py "$1" "${INPUT_DIRECTORY}" "$4" "${ACQ_NAME}"
+python /code/fw_upload_renderings.py "$1" "${INPUT_DIRECTORY}" "$4" "${acquisition_label}" "${session_label}" "${subject_label}"
